01b34b4a7f5565e1f56195dad76a0f0117ca1801
Ivanova Elena <ivanovaev90@gmail.com>
2015-05-02 22:51:21 +0300

    The example with ball was updated. Some new functions for fast animatin was added.

--- src/examples/01_canvasBall/js/main.js
+++ src/examples/01_canvasBall/js/main.js
@@ -17,8 +17,33 @@ Ball.prototype.draw = function(ctx) {
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var ball = new Ball(50, '#009AFF');
+var ball;
+var t;
 
-ball.x = 100;
-ball.y = 100;
-ball.draw(ctx);
+window.onload = init;
+
+function init() {
+    ball = new Ball(20, '#0000ff');
+    ball.x = 50;
+    ball.y = 250;
+    ball.vx = 2;
+    t = new Date().getTime();
+    ball.draw(ctx);
+    animFrame();
+};
+
+// setInterval(function() {
+//     onEachStep();
+// }, 1000 / 60);
+function animFrame() {
+    requestAnimationFrame(animFrame);
+    onEachStep();
+};
+
+function onEachStep() {
+    var dt = (new Date().getTime() - t) / 1000 * 60; // 1 секунда прошедшая после последнего вызова
+    t = new Date().getTime(); // reset t
+    ball.x += ball.vx * dt;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.draw(ctx);
+};
